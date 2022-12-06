80fa13afeae967b748de4e70590c74ca676a236d
zmwilliam <willcxa@gmail.com>
2022-12-06 03:46:21 -0300

    2022: Day 3 part 2

--- 2022/day-03/day_3.exs
+++ 2022/day-03/day_3.exs
@@ -1,8 +1,8 @@
 defmodule Day03 do
-  def item_priority(<<c::utf8>>) when c in ?a..?z, do: c - 96
-  def item_priority(<<c::utf8>>) when c in ?A..?Z, do: c - 38
+  defp item_priority(<<c::utf8>>) when c in ?a..?z, do: c - 96
+  defp item_priority(<<c::utf8>>) when c in ?A..?Z, do: c - 38
 
-  def compartment_items(rucksack_items) do
+  defp compartment_items(rucksack_items) do
     mid = rucksack_items |> String.length() |> div(2)
 
     rucksack_items
@@ -10,17 +10,37 @@ defmodule Day03 do
     |> Enum.split(mid)
   end
 
-  def shared_items({comp_one, comp_two}) do
+  defp shared_items({comp_one, comp_two}) do
     diff = List.myers_difference(comp_one, comp_two)
     Keyword.get(diff, :eq)
   end
 
-  def part_one() do
-    "input.txt"
+  defp read_file_lines(path) do
+    path
     |> File.read!()
     |> String.split("\n", trim: true)
+  end
+
+  def part_one() do
+    "input_example.txt"
+    |> read_file_lines()
     |> Enum.map(&compartment_items/1)
     |> Enum.map(&shared_items/1)
+    |> List.flatten()
+    |> Enum.map(&item_priority/1)
+    |> Enum.sum()
+  end
+
+  def part_two() do
+    "input.txt"
+    |> read_file_lines()
+    |> Enum.map(&String.codepoints/1)
+    |> Enum.chunk_every(3)
+    |> Enum.map(fn [a, b, c] ->
+      ab = a -- a -- b
+      ac = a -- a -- c
+      Enum.uniq(ab -- ab -- ac)
+    end)
     |> List.flatten()
     |> Enum.map(&item_priority/1)
     |> Enum.sum()
@@ -28,3 +48,4 @@ defmodule Day03 do
 end
 
 Day03.part_one() |> IO.inspect(label: "Part one")
+Day03.part_two() |> IO.inspect(label: "Part two")
