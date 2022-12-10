a98b656f1d3fac41ad53f1c53f35900e707c77d6
zmwilliam <willcxa@gmail.com>
2022-12-10 19:42:24 -0300

    2022: Day 6 part 2

--- 2022/day-06/tuning_trouble.exs
+++ 2022/day-06/tuning_trouble.exs
@@ -1,22 +1,23 @@
 defmodule TuningTrouble do
-  def find_marker(input) do
+  def find_marker(input, marker_size) do
     input
     |> String.graphemes()
     |> Enum.with_index(1)
-    |> Enum.chunk_every(4, 1, :discard)
+    |> Enum.chunk_every(marker_size, 1, :discard)
     |> Enum.find(fn quad ->
       uniq = Enum.uniq_by(quad, &elem(&1, 0))
-      Enum.count(uniq) == 4
+      Enum.count(uniq) == marker_size
     end)
     |> List.last()
     |> elem(1)
   end
 
-  def part_one do
+  def run(marker_size) do
     "input.txt"
     |> File.read!()
-    |> find_marker()
+    |> find_marker(marker_size)
   end
 end
 
-TuningTrouble.part_one() |> IO.inspect(label: "Part one")
+TuningTrouble.run(4) |> IO.inspect(label: "Part one")
+TuningTrouble.run(14) |> IO.inspect(label: "Part two")
--- 2022/day-06/tuning_trouble_test.exs
+++ 2022/day-06/tuning_trouble_test.exs
@@ -11,9 +11,20 @@ defmodule TuningTroubleTest do
     # nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg: first marker after character 10
     # zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw: first marker after character 11
 
-    assert 5 == TuningTrouble.find_marker("bvwbjplbgvbhsrlpgdmjqwftvncz")
-    assert 6 == TuningTrouble.find_marker("nppdvjthqldpwncqszvftbrmjlhg")
-    assert 10 == TuningTrouble.find_marker("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg")
-    assert 11 == TuningTrouble.find_marker("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw")
+    assert 5 == TuningTrouble.find_marker("bvwbjplbgvbhsrlpgdmjqwftvncz", 4)
+    assert 6 == TuningTrouble.find_marker("nppdvjthqldpwncqszvftbrmjlhg", 4)
+    assert 10 == TuningTrouble.find_marker("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg", 4)
+    assert 11 == TuningTrouble.find_marker("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw", 4)
+
+    # mjqjpqmgbljsphdztnvjfqwrcgsmlb:     first marker after character 19
+    # bvwbjplbgvbhsrlpgdmjqwftvncz:       first marker after character 23
+    # nppdvjthqldpwncqszvftbrmjlhg:       first marker after character 23
+    # nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg:  first marker after character 29
+    # zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw:   first marker after character 26
+    assert 19 == TuningTrouble.find_marker("mjqjpqmgbljsphdztnvjfqwrcgsmlb", 14)
+    assert 23 == TuningTrouble.find_marker("bvwbjplbgvbhsrlpgdmjqwftvncz", 14)
+    assert 23 == TuningTrouble.find_marker("nppdvjthqldpwncqszvftbrmjlhg", 14)
+    assert 29 == TuningTrouble.find_marker("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg", 14)
+    assert 26 == TuningTrouble.find_marker("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw", 14)
   end
 end
